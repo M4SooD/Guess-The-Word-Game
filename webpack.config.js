8dbd2548b2075f84ddec723eee2445bcffabd14b
atefeh199170 <72982087+amene-mi@users.noreply.github.com>
2021-10-26 20:25:13 +0330

    install webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "source", to: "dest" },
+        { from: "other", to: "public" },
+      ],
+    }),
+    new HtmlWebpackPlugin(),
+  ],
+};
