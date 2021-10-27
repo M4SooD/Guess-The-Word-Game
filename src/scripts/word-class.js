e3cd7acb78c80544f52af8b19b35ce0fa8b11b14
amene-mi <a_mohammadi1991@yahoo.com>
2021-10-27 15:50:16 +0330

    update word class

--- src/scripts/word-class.js
+++ src/scripts/word-class.js
@@ -1,27 +1,51 @@
-const words=['ameneh','masoud','Hello'];
+const words=['vein', 'velvet', 'artist', 'nerve', 'dare',
+    'cultivate', 'dynamic', 'contact', 'audience', 'thread', 'voucher', 'pan', 'faithful', 'confine', 'solution', 'tired',
+    'patience', 'jet', 'pavement', 'jealous', 'indulge', 'reach', 'heaven', 'gallery', 'comedy', 'wealth', 'kill', 'finance',
+    'sock', 'exaggerate', 'coast', 'replacement', 'lid', 'bedroom', 'debut', 'bottle', 'discovery', 'inject', 'curriculum',
+    'merit', 'distortion', 'frequency', 'grant', 'lazy', 'adoption', 'assertive', 'program', 'invisible', 'wing', 'hall'];
 class Word {
-    #word;
-    
-    constructor(level) {
-        this.level = level;
-        this.#word['correct']=words[Math.floor(Math.random()*words.length)];
-        if(level ==="Hard"){
-            randomHide(correct(),9);
+    #word ={};
+    #numberHiddenCharacters;
+    #numberWords;
+    #score;
+    filterArray;
+    constructor(score,numberWords) {
+       this.#numberWords= numberWords;
+        this.#score = score;
+
+        this.filterArray = words.filter(word => word.length <= this.#numberWords);
+    }
+
+    get generateWord(){
+        this.#word ={};
+        this.#word.correct= this.filterArray[Math.floor(Math.random()*this.filterArray.length)] ;
+        this.#word.withHidden = this.randomHide(this.#word.correct);
+        return this.#word;
+    }
+
+    randomHide(word) {
+        let text = [...word];
+        this.#word.withHidden=[];
+        this.#numberHiddenCharacters = Math.floor(text.length * 0.4) ;
+        for (let i= 0; i<this.#numberHiddenCharacters;i++){
+            this.#word.withHidden.push(text[Math.floor(Math.random()*text.length)]);
         }
-    }
-    get correct(){
-        return correct;
+        return this.#word.withHidden;
     }
 
-    randomHide([...correct],num) {
-      
+    get score(){
+        return this.#score * this.#numberHiddenCharacters;
     }
 
-    userTry([...char]){
-
+    userTry(char){
+        let inputUser= [...char];
+        let test = this.#word.withHidden;
+        if (inputUser.length === test.length && inputUser.every((v, i) => v === test[i])){
+            return  this.score;
+        }
+        else
+            return 0;
     }
 }
 
-let myWord = new Word('Hard');
-console.log( myWord.correct());
 module.exports = Word;
