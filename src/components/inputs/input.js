55f5f099d9948ce6dde9dcfcb50e16b5b0d69de4
amene-mi <a_mohammadi1991@yahoo.com>
2021-11-04 09:36:25 +0330

    change input function to class

--- src/components/inputs/input.js
+++ src/components/inputs/input.js
@@ -1,7 +1,16 @@
-function createInput(id) {
-    var input = document.createElement("input");
-    input.type = "text";
-    input.className = "input"; 
-    input.id = id;
-   // container.appendChild(input);
+class Input{
+    #input;
+    constructor(id,className,type,value){
+        this.#input = document.createElement("input");
+        this.#input.type = type;
+        this.#input.className = className; 
+        this.#input.id = id;
+        this.#input.value = value;
+    }
+
+    get id() {
+        return this.#input.id;
+    }
 }
+
+module.exports = Input;
