ddd62aa314314243648ed010a412e520737a1e73
amene-mi <a_mohammadi1991@yahoo.com>
2021-11-04 09:36:56 +0330

    import new js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+const Button = require('./components/buttons/button');
+const Div = require('./components/divs/div');
+const Input = require('./components/inputs/input');
 const WordsFactory = require("./scripts/words-factory-class");
 const Word = require('./scripts/word-class');
 const Game = require('./scripts/game-class');
