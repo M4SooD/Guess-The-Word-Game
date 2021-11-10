a9bee96a40053762f8609ff2b8a90cfe56a8ca56
amene-mi <a_mohammadi1991@yahoo.com>
2021-11-10 16:08:04 +0330

    update main js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 const Button = require('./components/buttons/button');
 const Div = require('./components/div/div');
 const Input = require('./components/inputs/input');
-const ComboBox = require('./components/dropdown/comboBox');
+const ComboBox = require('./components/dropdown/combo-box');
 const Heading = require('./components/heading/heading');
-const Page = require('./components/pages/stepOne');
+const PageOne = require('./components/pages/step-one');
+const PageTwo = require('./components/pages/step-two');
+const PageThree = require('./components/pages/step-three');
+const PageFour = require('./components/pages/step-four');
+
 const WordsFactory = require("./scripts/words-factory-class");
 const Word = require('./scripts/word-class');
 const Game = require('./scripts/game-class');
+
+const btnEvents = require('./components/buttons/events');
+const cmbEvents = require('./components/dropdown/events');
