c8b54d300f18b2a2635ad55d2f8390e80cff66d7
amene-mi <a_mohammadi1991@yahoo.com>
2021-11-10 16:06:08 +0330

    update Timer-class

--- src/scripts/timer-class.js
+++ src/scripts/timer-class.js
@@ -1,13 +1,30 @@
 class Timer {
-    #timer;
-    set timer(time){
-        this.#timer=time;
+    #time;
+    #timer; //10 will  run it every 100th of a second
+
+    constructor(time) {
+        this.#time = time;
+        this.#timer = time;
     }
-    get RemainingTime() {
+
+    set timer(time) {
+        this.#time = time;
+    }
+
+    downloadTimer(id,game, stepper) {
+        if (this.#timer === 0) {
+            stepper.goToNextStep(game,stepper);
+            clearInterval() ;
+        }
+        document.getElementById(id).value = this.#time - this.#timer;
+        this.#timer -= 1;
+    }
+
+    RemainingTime() {
 
     }
 
-    memorisingTimer() {
+    memorizingTimer() {
 
     }
 
