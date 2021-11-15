73219e5a87c7ce145d6022e1701bc8ee684b10cf
amene-mi <a_mohammadi1991@yahoo.com>
2021-11-15 15:17:34 +0330

    update input

--- src/components/inputs/input.js
+++ src/components/inputs/input.js
@@ -1,19 +1,21 @@
-class Input{
+class Input {
     #input;
-    constructor(id,className,type,placeholder,value){
+    constructor(id, className, type, placeholder, value, minLength, maxLength) {
         this.#input = document.createElement("input");
         this.#input.type = type;
-        this.#input.classList.add(...className); 
+        this.#input.classList.add(...className);
         this.#input.id = id;
         this.#input.placeholder = placeholder;
         this.#input.value = value;
+        this.#input.minLength = minLength;
+        this.#input.maxLength = maxLength;
     }
 
     get id() {
         return this.#input.id;
     }
 
-    get build(){        
+    get build() {
         return this.#input;
     }
 }
